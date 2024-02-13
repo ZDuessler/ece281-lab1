3d5e28d312fc6ec947634d32ad0f73336751686a
Duessler <c26zachary.duessler@afacademy.af.edu>
2024-02-12 21:44:32 -0700

    generates proper waveform, missing bitstream to board and doc statement

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -56,10 +56,10 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
-						-- output
+	i_B : in std_logic;
+	i_C : in std_logic; 
+    i_D : in std_logic;
+	o_Y : out std_logic					-- output
   );
 end thirtyOneDayMonth;
 
@@ -67,13 +67,18 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 	-- include components declarations and signals
 	
 	--signals internal to the architecture are declared and initialized such as w_sel
-  
+    signal w_sel : std_logic_vector (3 downto 0); -- MUX sel
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
+	w_sel(0) <= i_A;	-- one
+	w_sel(1) <= i_B; -- connect input C to the LSB of w_sel
+	w_sel(2) <= i_C; -- connect input C to the LSB of w_sel
+	w_sel(3) <= i_D;
 	--finish assigning signals
-	
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	o_Y <= i_D when (i_A = '0') else 
+           not(i_D) when (i_A = '1') else            
+           '0';
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
